77308401e633fdd674d4c27651de16ed90495702
Wiam Salaheldin <69213850+WiamSalaheldin@users.noreply.github.com>
2020-11-13 19:14:17 -0500

    Update library.py

--- library.py
+++ library.py
@@ -1,6 +1,8 @@
-def sum(numbers):
-    total = 0
-    for x in numbers:
-        total += x
-    return total
+# Function for Fibonacci number
+# Fibunatcci numbers {0,\;1,\;1,\;2,\;3,\;5,\;8,\;13,\;21,\;34,\;55,\;89,\;144,....}
+# Fibunatcci formula  F_{n}=F_{n-1}+F_{n-2}}
+def F(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return F(n-1)+F(n-2)
 
